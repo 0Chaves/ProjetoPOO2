e700bf6520d904e3099442a8517e3d52b2a669fb
0Chaves <danieldossantoschaves@gmail.com>
2024-12-10 15:40:38 -0300

    Feat: tela de listagem de fornecedores

--- src/main/java/Chaves/ProjetoLicitacoes/controller/TelaInserirItemController.java
+++ src/main/java/Chaves/ProjetoLicitacoes/controller/TelaInserirItemController.java
@@ -1,17 +1,38 @@
 package Chaves.ProjetoLicitacoes.controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
 
 public class TelaInserirItemController {
+	private Stage stage;
+	private Scene scene;
+	private AnchorPane root;
+	
 
     @FXML
     private Button botao_voltar;
 
     @FXML
     void voltar(ActionEvent event) {
-
+    	try {
+			root = (AnchorPane)FXMLLoader.load(getClass().getResource("/Chaves/ProjetoLicitacoes/views/TelaInicial.fxml"));
+			scene = new Scene(root);
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
     }
 
 }
---
+++ src/main/java/Chaves/ProjetoLicitacoes/controller/TelaListaFornecedoresController.java
@@ -0,0 +1,60 @@
+package Chaves.ProjetoLicitacoes.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TelaListaFornecedoresController {
+
+    @FXML
+    private Button Abrir;
+
+    @FXML
+    private Button Deletar;
+
+    @FXML
+    private Button Editar;
+
+    @FXML
+    private Button Pesquisar;
+
+    @FXML
+    private Button Voltar;
+
+    @FXML
+    private TableView<?> tabela;
+
+    @FXML
+    private TextField txtCnpj;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    void abrir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+
+}
